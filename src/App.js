006e4d93aa058d5761ac6d962e19a2c9a422ce00
fogelo <fogelo@yandex.ru>
2021-08-21 10:27:58 +0700

    l11

--- src/App.js
+++ src/App.js
@@ -3,20 +3,31 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
-      <Header></Header>
+      <header className = 'header'>
+        <img className = 'header-img'src ='https://www.lifepng.com/wp-content/uploads/2020/10/580b57fcd9996e24bc43c53e.png'></img>
+      </header>
+      <nav className = 'navbar'>
+        <div><a>Profile</a></div>
+        <div><a>Messages</a></div>
+        <div><a>News</a></div>
+        <div><a>Music</a></div>
+        <div><a>Settings</a></div>
+      </nav>
+
+      <div className = 'main-content'>
+        <div>ava+description</div>
+        <div>
+          My Posts
+          <div>New post</div>
+          <div>Post 1</div>
+          <div>Post 2</div>
+          <div>Post 3</div>
+          <div>Post 4</div>
+          <div>Post 5</div>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
-
-const Header = () => {
-  return (
-    <div className="App">
-       Здарова епта! Работа с git. Мы внесли определенные изменения
-       загрузили это все дело на гитхаб
-    </div>
-  );
-}
-
-
