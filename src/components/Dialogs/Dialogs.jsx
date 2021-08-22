832fa089a0082b05b6ae9a15c0fb096079c3310b
fogelo <fogelo@yandex.ru>
2021-08-23 00:01:52 +0700

    l21

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,7 +2,20 @@ import classes from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-        <div>Dialogs</div>
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={classes.dialog +' '+ classes.active}>Sveta</div>
+                <div className={classes.dialog}>Andrey</div>
+                <div className={classes.dialog}>Anna</div>
+                <div className={classes.dialog}>Dmitriy</div>
+                <div className={classes.dialog}>Vladimir</div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>What do you do?</div>
+                <div className={classes.message}>Where are you?</div>
+                <div className={classes.message}>What is your name?</div>
+            </div>
+        </div>
     )
 }
 
