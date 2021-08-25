aff4386e937aecfb60b78dd88be2f4ec001cf967
fogelo <fogelo@yandex.ru>
2021-08-25 20:32:49 +0700

    l31 onClick, ref, VirtualDOM

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import classes from './Dialogs.module.css'
 import DialogsItem from './DialogsItem/DialogsItem'
 import Message from './Message/Message'
@@ -9,6 +10,12 @@ const Dialogs = (props) => {
     let dialogsElements = props.dialogs.map(d => <DialogsItem name={d.name} id={d.id}></DialogsItem>) 
     let messagesElements = props.messages.map(m => <Message message={m.message}></Message>)
     
+    let newMessageElement = React.createRef();
+    let addMessage = ()=>  {
+        let message = newMessageElement.current.value;
+        alert(message);
+    }
+
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogsItems}>
@@ -17,6 +24,9 @@ const Dialogs = (props) => {
             <div className={classes.messages}>
                 {messagesElements}
             </div>
+
+            <textarea ref={newMessageElement}></textarea>
+            <button onClick={addMessage}>add</button>
         </div>
     )
 }
--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,15 +1,22 @@
+import React from 'react';
 import classes from './ProfileInfo.module.css'
 
-
-
 const ProfileInfo = () => {
+
+  let newPostElement = React.createRef();
+
+  let addPost = () => {
+    let text = newPostElement.current.value;
+    alert(text);
+  }
+
   return (
     <div className={classes.ProfileInfo}>
       <div>ava+description</div>
       <h3>My Posts</h3>
       <div>
-        <textarea></textarea>
-        <div><button>add</button></div>
+        <textarea ref={newPostElement}></textarea>
+        <div><button onClick={addPost}>add</button></div>
       </div>
     </div>
   )
