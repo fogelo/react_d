a77e323a296eb308ba7734f9d870836070e1b4b9
fogelo <antonorlov@MacBook-Pro-Anton.local>
2021-11-04 22:05:45 +0300

    l51 подключил библиотеку axios, сделал запрос на сервер для получения users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,24 +1,25 @@
 import classes from './Users.module.css'
-
+import * as axios from 'axios' // импортируем все(*) что находится в библиотеке 'axios' в обьект axios
+import userPhoto from '../../assets/images/photo.jpeg'
 
 
 let Users = (props) => {
 
-  if(props.users.length === 0) {
-    props.setUsers([
-        { id: 1, photoUrl: 'https://www.vokrug.tv/pic/person/4/c/3/8/4c385380340d56acd318cf77d7777924.jpeg', followed: false, fullName: 'Dmitriy', status: 'I am a bose', location: { city: 'Minsk', country: 'Belarus' } },
-        { id: 2, photoUrl: 'https://www.vokrug.tv/pic/person/4/c/3/8/4c385380340d56acd318cf77d7777924.jpeg', followed: true, fullName: 'Sasha', status: 'I am a bose too', location: { city: 'Moscow', country: 'Russia' } },
-        { id: 3, photoUrl: 'https://www.vokrug.tv/pic/person/4/c/3/8/4c385380340d56acd318cf77d7777924.jpeg', followed: false, fullName: 'Andrew', status: 'I am a bose too', location: { city: 'Kiev', country: 'Ukraine' } },])
-  }
+    if (props.users.length === 0) { // такую грязь делать нельзя, в помощь классовые компоненты (l52)
+
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            props.setUsers(response.data.items)
+        })
+    }
 
     return <div>
         {
             props.users.map(u => <div key={u.id}>
-                <div><img className={classes.photo} src={u.photoUrl}></img></div>
-                <div>{u.fullName}</div>
+                <div><img className={classes.photo} src={u.photos.small != null ? u.photos.small : userPhoto}></img></div>
+                <div>{u.name}</div>
                 <div>{u.status}</div>
-                <div>{u.location.city}</div>
-                <div>{u.location.country}</div>
+                <div>{'u.location.city'}</div>
+                <div>{'u.location.country'}</div>
                 <div>
                     {u.followed
                         ? <button onClick={() => { props.unfollow(u.id) }}>unfollow</button>
