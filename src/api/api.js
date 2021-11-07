dc1ab79e56a4f95f75f86157c1e2ca7666a25805
fogelo <antonorlov@MacBook-Pro-Anton.local>
2021-11-07 19:44:19 +0300

    l63 добавил DAL уровень, создал фпапку api и вынес в нее запросы

---
+++ src/api/api.js
@@ -0,0 +1,21 @@
+import * as axios from 'axios' // импортируем все(*) что находится в библиотеке 'axios' в обьект axios
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        "API-KEY": "dbb2362b-785b-4f9a-847a-900f168fe50a"
+    },
+    
+
+})
+
+export const usersAPI = {
+    getUsers(currentPage = 1, pageSize = 10) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+            .then(response => {
+                return response.data
+            });
+    }
+
+}
--- src/components/Users/UsersContanier.jsx
+++ src/components/Users/UsersContanier.jsx
@@ -1,33 +1,28 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { follow, setUsers, unfollow, setCurrentPage, setTotalUsersCount, toggleIsFetching } from '../../redux/users-reducer';
-import * as axios from 'axios' // импортируем все(*) что находится в библиотеке 'axios' в обьект axios
 import Users from './Users'
 import Preloader from '../common/Preloader/Preloader';
+import { usersAPI } from '../../api/api';
+
 
 class UsersContanier extends React.Component {
     componentDidMount() {
         this.props.toggleIsFetching(true)
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`, {
-            withCredentials: true,
+        usersAPI.getUsers(this.props.currentPage, this.props.pageSize).then(data => {
+            this.props.toggleIsFetching(false)
+            this.props.setUsers(data.items)
+            this.props.setTotalUsersCount(data.totalCount)
         })
-            .then(response => {
-                this.props.toggleIsFetching(false)
-                this.props.setUsers(response.data.items)
-                this.props.setTotalUsersCount(response.data.totalCount)
-            })
     }
 
     onPageChanged = (pageNumber) => {
         this.props.setCurrentPage(pageNumber)
         this.props.toggleIsFetching(true)
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`, {
-            withCredentials: true,
+        usersAPI.getUsers(pageNumber, this.props.pageSize).then(data => {
+            this.props.toggleIsFetching(false)
+            this.props.setUsers(data.items)
         })
-            .then(response => {
-                this.props.toggleIsFetching(false)
-                this.props.setUsers(response.data.items)
-            })
     }
     render() {
         return <>
