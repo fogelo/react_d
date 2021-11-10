f3058a98da8533c7a180578f961de5bf15293f4f
fogelo <antonorlov@MacBook-Pro-Anton.local>
2021-11-10 13:40:15 +0300

    l74 подключил метод componentDidUpdate для перерисовки статуса

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -22,8 +22,14 @@ class ProfileStatus extends React.Component {
             status: e.currentTarget.value
         })
     }
+    componentDidUpdate(prevProps, prevState) {
+        if (prevProps.status !== this.props.status) {
+            this.setState({ status: this.props.status })
+        }
+    }
 
     render() {
+
         return (
             <div> <span>  </span>
                 {!this.state.editMode &&
